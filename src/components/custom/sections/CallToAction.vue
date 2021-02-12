89a76f34dcc604d826af633cd87c37355b17ddb9
Vilnite <vilnitekristine@gmail.com>
2021-02-12 17:41:30 +0200

    cta update

--- src/components/custom/sections/CallToAction.vue
+++ src/components/custom/sections/CallToAction.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="pb-20 px-4 bg-cover text-center" :style="img">
-    <br>
     <div class="w-full max-w-7xl mx-auto">
       <h2 class="text-4xl leading-tight font-semibold font-heading">
         {{ cta.title }}
@@ -9,7 +8,6 @@
         v-html="cta.content"
         class="mt-6 mb-8 text-gray-700 leading-relaxed"
       ></div>
-
 
        <g-link
           class="bg-blue-900 text-sm learn-button hover:bg-blue-800 text-gray-100 px-12 py-2 mr-5 rounded shadow rounded-full" 
@@ -29,7 +27,6 @@
           :to="cta.link3"
           >{{ cta.button3 }}</g-link
         >
-      
       
     </div>
   </section>
